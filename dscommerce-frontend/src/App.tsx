df510f1f0dd73c16f2d26d1e0c7dc7fc5d52d29d
auriceliof <auricelio.suporte@hotmail.com>
2023-12-15 20:16:48 -0300

    02-12 Criando o componente ProductDetails

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,24 +1,8 @@
-import './App.css'
-import ButtonInverse from './components/ButtonInverse';
-import ButtonPrimary from './components/ButtonPrimary';
-import ProductDetailsCard from './components/ProductDetailsCard';
-import HeaderClient from './components/HeaderClient';
+import ProductDetails from './routes/ProductDetails';
 
 function App() {
   return (
-    <>
-      <HeaderClient />
-      <main>
-        <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
-
-          <div className="dsc-btn-page-container">
-            <ButtonPrimary />
-            <ButtonInverse />
-          </div>
-        </section>
-      </main>
-    </>
+    <ProductDetails />
   );
 }
 
