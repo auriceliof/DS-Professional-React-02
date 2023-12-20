c1a8c7103eb2e92ac79c1f48e3fda99fb833366f
auriceliof <auricelio.suporte@hotmail.com>
2023-12-20 11:11:16 -0300

    02-15 Carros Top exercício resolvido, começando

---
+++ carrostop/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './Styles.css';
+
+export default function Header() {
+
+    return (
+        <header>
+            <div>
+                <h1>Carros Top</h1>
+            </div>
+        </header>
+    );
+}
---
+++ carrostop/src/routes/Catalog/index.tsx
@@ -0,0 +1,9 @@
+import Header from '../../components/Header';
+import './Styles.css';
+
+export default function Catalog() {
+
+    return (
+        <Header />
+    );
+}
